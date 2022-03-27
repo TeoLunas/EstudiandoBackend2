25206e9b5874f4098b6d98b99842c522a11be464
TeoLunas <lunasteobaldo@gamil.com>
2022-03-27 00:31:21 -0300

    Crea primera ruta de home, ademas de router y controllers

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
+const userRouter = require('./routes/userRouter');
+
 const app = express();
 
 const port = 4000;
+
+//Router
+app.use('/api', userRouter)
 
 app.listen(port, () => {
     console.log(`App Corriendo en puerto ${port}`);
